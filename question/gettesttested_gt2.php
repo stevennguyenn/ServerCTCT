6cf846900c39b1d4996b5147ed62cdb0a836ed1a
steve <1510821@hcmut.edu.vn>
2018-12-07 00:19:05 +0700

    fix server get testted

---
+++ question/gettesttested_gt2.php
@@ -0,0 +1,29 @@
+<?php
+	require "C:\\xampp\htdocs\CTCT\connect.php";
+	require "tested.php";
+
+	$id = $_POST['id'];
+	$query = "SELECT test.test_code,studenttest.point,test.name,test.number_test 
+			  FROM studenttest 
+			  LEFT JOIN test using(test_code)
+			  WHERE test_code LIKE 'GT2\_%' AND point != 0 AND id = '$id'";
+
+	$data = mysqli_query($con,$query);
+	if ($data){
+		$result = array();
+		while ($row = mysqli_fetch_assoc($data)) {
+			$temp = $row['test_code'];
+			$query_get_rate = "SELECT get_rate($id,'$temp') as get_rate";
+			$get_rate = mysqli_query($con,$query_get_rate);
+			if ($get_rate){
+				$rate = mysqli_fetch_assoc($get_rate)['get_rate'];
+				array_push($result, new Tested($row['test_code'],$row['point'],$rate,$row['name'],$row['number_test']));
+			}
+		}
+		if (count($result) > 0 ){
+			echo json_encode($result);
+			return;
+		}
+	}
+	echo "Failed";
+?>
---
+++ question/tested.php
@@ -0,0 +1,12 @@
+<?php
+	class Tested{
+		function Tested($test_code,$point,$rate,$name,$number_test){
+			$this ->test_code = $test_code;
+			$this ->point = $point;
+			$this ->rate = $rate;
+			$this ->name = $name;
+			$this ->number_test = $number_test;
+		}
+	}
+
+?>
