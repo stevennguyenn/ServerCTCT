0a45f1784420fabe0b685e7bfeba2fdc10535fc5
steve <1510821@hcmut.edu.vn>
2018-12-08 00:40:22 +0700

    fix bug offline failed

--- question/gettitleoffline_gt1.php
+++ question/gettitleoffline_gt1.php
@@ -2,7 +2,11 @@
 	require "C:\\xampp\htdocs\CTCT\connect.php";
 	require "title.php";
 
-	$query = "SELECT test_code,name,number_test from test where test_status = 0 and test_code like 'GT1\_%'";
+	$id = $_POST['id'];
+
+	$query = "SELECT test_code,name,number_test 
+			  FROM test 
+              WHERE test_code NOT IN (select test_code from studenttest where id = '$id') and test_code like 'GT1\_%' AND test_status = 0";
 	$data  = mysqli_query($con,$query);
 
 	if ($data){
@@ -11,7 +15,7 @@
 			array_push($result, new Title($row['test_code'],$row['name'],$row['number_test']));
 		}
 
-		if (count($result) > 0){
+		if (count($result) >= 0){
 			echo json_encode($result);
 			return;
 		}
